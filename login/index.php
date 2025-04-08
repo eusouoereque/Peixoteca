39d1a69d70edaff4f66d82c7ba452984c49a4bc1
Vinícius Toshio <vtoshio2004@gmail.com>
2025-04-07 22:12:05 -0300

    Teste na página de login.

--- login/index.php
+++ login/index.php
@@ -1,10 +1,15 @@
 <?php 
     include "../components/header.php";
+    include "../components/nav.php";
     require_once "../db/conn.php";
 ?>
 
 
-
+<div class="container">
+    <div class="rounded text-light bg-primary">
+        <h1>Hello World!</h1>
+    </div>
+</div>
 
 
 
