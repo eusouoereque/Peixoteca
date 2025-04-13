1120025fef52977a1a13f7eff193bdb477394d3d
Vinícius Toshio <125375660+vToshio@users.noreply.github.com>
2025-04-12 21:50:56 -0300

    Funcionalidade de Logout (#4)
    * Teste na página de login.
    
    * Lógica da página de login, modificação de redirecionamento na navbar e adição do helpers.php
    
    * Criação da pagina de Cadastro de usuários
    
    * Correção da pagina de cadastros de usuarios
    
    * Alteração de mensagem de erro e sucesso
    
    * Adição da funcionalidade de logout
    
    ---------
    
    Co-authored-by: eusouoereque <eric.henriquep3@hotmail.com>
    Co-authored-by: Eric Henrique <106106727+eusouoereque@users.noreply.github.com>

--- components/nav.php
+++ components/nav.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse " id="navbarNavDropdown">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active text-light fs-5" aria-current="page" href="../animais/">Animais</a>
@@ -17,9 +17,19 @@
         </li>
       </ul>
       <div class="d-flex">
-        <a href="../login/index.php">
-          <button type="button" class="btn btn-outline-light fs-5">Login</button>
-        </a>
+        <?php
+          require_once '../helpers.php';
+          if (!usuarioEstaLogado()) {
+            echo '<a href="../login/">
+                    <button type="button" class="btn btn-outline-light fs-5">Login</button>
+                  </a>';
+          } else {
+            echo '<p class="text-light m-auto px-3 d-none d-md-flex ">'.$_SESSION['usuario_logado'].'</p>';
+            echo '<a href="../login/logout.php">
+                    <button type="button" class="btn btn-outline-danger fs-5">Logout</button>
+                  </a>';
+          }
+        ?>
       </div>
     </div>
   </div>
--- usuarios/cadastroUser.php
+++ usuarios/cadastroUser.php
@@ -1,6 +1,5 @@
 
 <?php
-session_start();
 require_once "../db/conn.php";
 require "../components/header.php";
 
